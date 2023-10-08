bf84d1f8676ba4b794a152e8bb9afa1f77b9d0c6
aldo armenta <AldoGuadalupe.Armenta@zkw.mx>
2023-10-07 22:41:01 -0600

    termiando registro de usuario e impresora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,17 +30,22 @@ export class AppComponent implements OnInit{
 
   
   printers: printers[] = [];
-  code: any;
+  arrayCodes: any;
+  arrRegistry:any;
   duplicateRegistry:any;
 
   selectedPrinter: any;
   email:string = '';
+  registryCode!: number;
 
   registry:any[] = [];
 
   maxSelections = 3;
+
   printerInput: boolean = false;
   emailInput: boolean = false;
+
+
   constructor(
     private messageService: MessageService, 
     private clipboardService: ClipboardService,
@@ -49,6 +54,9 @@ export class AppComponent implements OnInit{
     ) {}
   ngOnInit() {
     this.getPrinters();
+    this.getCodes();
+    this.getRegistry();
+    //this.duplicateRegistryCheck();
       
   }
   onChange(event: any) {
@@ -59,17 +67,7 @@ export class AppComponent implements OnInit{
       this.messageService.add({ severity: 'info', detail: 'solo se puede seleccionar 3 impresoras como maximo' });
     }
   }
-  randomCode = () => {
-    const codigoLength = 5;
-    let codigo = '';
-  
-    for (let i = 0; i < codigoLength; i++) {
-      const digitoAleatorio = Math.floor(Math.random() * 10); // Números aleatorios del 0 al 9
-      codigo += digitoAleatorio.toString();
-    }
-  
-    return codigo;
-  }
+/****get metods****/
 
   getPrinters = () => {
     const url = urlAPI+'/printers';
@@ -85,11 +83,11 @@ export class AppComponent implements OnInit{
     ).subscribe();
 
   }
-  getCodes = () => {
+  getCodes(){
     const url = urlAPI+'/duplicateCodes';
     this.http.get<any>(url).pipe(
       tap((data) => {
-
+        this.arrayCodes = data;
       }),
       catchError((error: any)=>{
         console.error(error);
@@ -97,10 +95,65 @@ export class AppComponent implements OnInit{
       })
     ).subscribe();
   }
+
+  getRegistry(){
+const url = urlAPI+'/duplicateRegistry';
+this.http.get<any>(url).pipe(
+  tap((data) => {
+    console.log(data);
+    this.arrRegistry = data;
+  }),
+  catchError((error: any) => {
+    console.error(error);
+    return of (null);
+  })
+).subscribe();
+
+  }
+  
+
+
+/*****POST METHODS******/
+
+
+  generateRegistry(){
+    if(this.email === '' || this.selectedPrinter === undefined){
+      this.errorMessage();
+    }else{
+      
+    const code = parseInt(this.generateUniqueCode());
+
+    this.registry = this.selectedPrinter.map((printer: printers) => ({
+      code: code,
+      id_printer: printer.id_printer,
+      email: this.email
+    }));
+
+    if(this.duplicateRegistryCheck()){
+      this.messageService.add({severity:'warn', detail:'Usuario ya existente, no se puede volver a registrar.'});
+    }else{
+      
+    this.clipboardService.copy(code.toString());
+    this.messageService.addAll([
+      { severity: 'success', detail: 'Registro exitoso' },
+      { severity: 'info', detail: 'codigo copiado al portapapeles' }
+    ]);
+
+      this.validateInputs();
+      this.sendRegistries();
+      this.clear();
+      
+      
+      }
+    }
+  }
+
   sendRegistries() {
     this.registryService.createRegistry(this.registry).subscribe({
       next: response => {
         console.log(response);
+          this.getRegistry();
+        
       },
       error: error => {
         console.error('Error al registrar', error);
@@ -108,34 +161,66 @@ export class AppComponent implements OnInit{
     });
   }
 
-  generateRegistry = () => {
-    if(this.email === '' || this.selectedPrinter === undefined){
+  /***********helper methods*************/
+// Función para generar un código aleatorio de 5 dígitos
+randomCode = (): string => {
+  const codigoLength = 5;
+  let codigo = '';
 
-      this.selectedPrinter === undefined || [] ? this.printerInput = true : this.printerInput = false; 
-      this.email === ''? this.emailInput = true: this.emailInput = true;
-      
-
-      this.messageService.add({ severity: 'error', detail: 'ingresa los datos solicitados' });
-    }else{
-
-    const code = parseInt(this.randomCode());
-    
-    this.registry = this.selectedPrinter.map((printer: printers) => ({
-      code: code,
-      id_printer: printer.id_printer,
-      email: this.email
-    }));
-
-    this.clipboardService.copy(code.toString());
-    this.messageService.addAll([
-      { severity: 'success', detail: 'Registro exitoso' },
-      { severity: 'info', detail: 'codigo copiado al portapapeles' }
-    ]);
-      this.emailInput == true ? this.emailInput=false : null;
-      this.printerInput == true ? this.printerInput = false : null;
-      console.log(this.registry);
-      this.sendRegistries();
-      
-    }
+  for (let i = 0; i < codigoLength; i++) {
+    const digitoAleatorio = Math.floor(Math.random() * 10); // Números aleatorios del 0 al 9
+    codigo += digitoAleatorio.toString();
   }
+
+  return codigo;
+}
+
+// Función para verificar si un código ya existe en la base de datos
+codeExists = (code: string, arrayCodes: string[]): boolean => {
+  return  arrayCodes.includes(code);
+}
+
+// Función para generar un código único y no duplicado
+generateUniqueCode = (): string => {
+  let newCode: string;
+
+  do {
+    newCode = this.randomCode();
+  } while (this.codeExists(newCode, this.arrayCodes));
+
+  return newCode;
+}
+duplicateRegistryCheck() {
+  const unDuplicateRegistry: any[] = [];
+  this.registry.forEach((reg: any) => {
+    const duplicates = this.arrRegistry.filter((registro: any) => registro.id_printer === reg.id_printer && registro.id_user === reg.id_user);
+    
+    // Si hay duplicados ejecutar la siguiente validacion
+    
+        // Si no hay duplicados, agrega el registro a filteredRegistry
+        if (duplicates.length === 0) {
+          unDuplicateRegistry.push(reg);
+        }
+  });
+  this.registry = unDuplicateRegistry;
+  
+return this.registry.length === 0 ?  true : false ;
+ //return 'h'; 
+}
+
+
+errorMessage(){
+  this.printerInput = this.selectedPrinter === undefined || this.selectedPrinter.length === 0;
+  this.emailInput = this.email === '';
+  this.messageService.add({ severity: 'error', detail: 'ingresa los datos solicitados' });
+}
+validateInputs(){
+  this.emailInput == true ? this.emailInput=false : null;
+  this.printerInput == true ? this.printerInput = false : null;
+}
+clear(){
+  this.selectedPrinter = [];
+  this.email = '';
+}
+
 }
